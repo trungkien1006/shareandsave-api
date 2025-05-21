fe89a29b28a0e2837ae44f02984f90d1700b9658
trungkien1006 <cristntk0162@gmail.com>
2025-05-22 00:48:42 +0700

    update db connection function

--- internal/boostraps/db.go
+++ internal/boostraps/db.go
@@ -2,6 +2,7 @@ package boostraps
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -21,13 +22,12 @@ func GormConnection() *gorm.DB {
 		devPort     = os.Getenv("MYSQL_PORT")
 	)
 
+	// In ra để kiểm tra nếu cần debug
 	fmt.Println("MYSQL_HOST:", devHostName)
 	fmt.Println("MYSQL_PORT:", devPort)
 	fmt.Println("MYSQL_USER:", devUser)
 	fmt.Println("MYSQL_PASSWORD:", devPassword)
 	fmt.Println("MYSQL_NAME:", devDbName)
-
-	var err error
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		devUser,
@@ -37,9 +37,11 @@ func GormConnection() *gorm.DB {
 		devDbName,
 	)
 
+	var err error
+
 	// Retry tối đa 10 lần, mỗi lần cách nhau 5s
 	for i := 0; i < 10; i++ {
-		db, err := gorm.Open(
+		db, err = gorm.Open(
 			mysql.Open(dsn),
 			&gorm.Config{
 				NamingStrategy: schema.NamingStrategy{
@@ -48,25 +50,15 @@ func GormConnection() *gorm.DB {
 			},
 		)
 		if err == nil {
-			fmt.Println("Kết nối MySQL thành công!")
+			fmt.Println("✅ Kết nối MySQL thành công!")
 			return db
 		}
-		fmt.Printf("Không thể kết nối MySQL, thử lại sau 5s (%d/10)...\n", i+1)
+		fmt.Printf("❌ Không thể kết nối MySQL, thử lại sau 5s (%d/10)...\n", i+1)
 		fmt.Println("DSN:", dsn)
 		time.Sleep(5 * time.Second)
 	}
-	// db, err := gorm.Open(
-	// 	mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", devUser, devPassword, devHostName, devPort, devDbName)+"?parseTime=true&charset=utf8mb4&loc=Local"),
-	// 	&gorm.Config{
-	// 		NamingStrategy: schema.NamingStrategy{
-	// 			SingularTable: true,
-	// 		},
-	// 	},
-	// )
 
-	if err != nil {
-		panic(err)
-	}
-
-	return db
+	// Sau 10 lần vẫn lỗi thì panic
+	log.Fatalf("Lỗi khi kết nối MySQL sau nhiều lần thử: %v", err)
+	return nil
 }
