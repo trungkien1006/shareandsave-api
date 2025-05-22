9ef05610d42f9b27571f8a66c8b4f9420bd1586f
trungkien1006 <cristntk0162@gmail.com>
2025-05-23 01:58:36 +0700

    fix: validate rule of GetUserRequest model

--- internal/dto/userDTO/request.go
+++ internal/dto/userDTO/request.go
@@ -1,9 +1,9 @@
 package userDTO
 
 type GetUserRequest struct {
-	Page   int    `query:"page" binding:"min:1"`
-	Limit  int    `query:"limit" binding:"min:1"`
-	Sort   string `query:"sort" binding:"oneof:ASC DESC"`
+	Page   int    `query:"page" binding:"min=1"`
+	Limit  int    `query:"limit" binding:"min=1"`
+	Sort   string `query:"sort" binding:"oneof=ASC DESC"`
 	Order  string `query:"order"`
 	Filter string `query:"filter"`
 }
