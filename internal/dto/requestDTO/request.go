abe777c277512685a677e4996c203975ecd3117a
trungkien1006 <cristntk0162@gmail.com>
2025-05-28 14:20:17 +0700

    fix(request): Fix lỗi validate của request model

--- internal/dto/requestDTO/request.go
+++ internal/dto/requestDTO/request.go
@@ -10,7 +10,7 @@ type CreateRequestSendOldItem struct {
 	PhoneNumber         string    `json:"phoneNumber" example:"0123456789"`
 	UserID              uint      `json:"userId"`
 	Description         string    `json:"description" example:"I want to send this old item to the charity organization."`
-	IsAnonymous         bool      `json:"isAnonymous" binding:"oneof=true false"`                                      // true: anonymous, false: not anonymous
+	IsAnonymous         bool      `json:"isAnonymous"`                                                                 // true: anonymous, false: not anonymous
 	AppointmentTime     time.Time `json:"appointmentTime" binding:"required" example:"2025-05-28T07:23:45Z"`           // Time in RFC3339 format
 	AppointmentLocation string    `json:"appointmentLocation" binding:"required" example:"123 Main St, City, Country"` // Location of the appointment
 }
