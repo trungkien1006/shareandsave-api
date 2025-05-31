7a21814870b3bf786d1c0aa4eca5c06c5f499308
trungkien1006 <cristntk0162@gmail.com>
2025-05-31 18:58:40 +0700

    chores(post): Cập nhật example data cho request model

--- internal/dto/postDTO/post_request.go
+++ internal/dto/postDTO/post_request.go
@@ -12,5 +12,5 @@ type CreatePostRequest struct {
 	Type        enums.PostType `json:"type" oneof:""`
 	Title       string         `json:"title" binding:"required" example:"Tôi muốn tìm đồ thất lạc"`
 	Info        string         `json:"info" binding:"required" example:"{'a': 'oke', 'b': 'hihi'}"`
-	Images      []string       `json:"images" example:"['strbase64', 'strbase64']"`
+	Images      []string       `json:"images" example:"strbase64, strbase64"`
 }
